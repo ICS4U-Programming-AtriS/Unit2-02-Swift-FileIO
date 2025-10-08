4f2a162c0cc034c1791f195be73186910ce23dc1
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-10-08 03:52:17 +0000

    WORKING!

--- FileIO.swift
+++ FileIO.swift
@@ -24,12 +24,70 @@ guard let inputFile = FileHandle(forReadingAtPath: inputFilePath) else {
     exit(1)
 }
 
-// Access the output file
-guard let outputFile = FileHandle(forReadingAtPath: outputFilePath) else {
-    print("CANNOT OPEN OUTPUT FILE")
+// Access the output file for writing
+guard let outputFile = FileHandle(forWritingAtPath: outputFilePath) else {
+    print("CANNOT OPEN OUTPUT FILE FOR WRITING")
     exit(1)
 }
 
+
+// Helper function for writing data
+func writeToOutputFile(_ text: String) {
+    if let data = text.data(using: .utf8) {
+        outputFile.write(data)
+    } else {
+        print("Error: WRITING FAILED")
+    }
+}
+
 // Read the contents of the input file
-let inputData = 1
+let inputData = inputFile.readDataToEndOfFile()
+
+// Convert the data to a string
+guard let inputString = String(data: inputData, encoding: .utf8) else {
+    print("CANNOT CONVERT FILE DATA TO A STRING")
+    exit(1)
+}
+
 // Create a list of all available lines
+let listOfLines = inputString.components(separatedBy: .newlines)
+
+// Loop through all the lines in the list
+for line in listOfLines {
+    // Check if the line is empty
+    if line == "" {
+        // If so, write an error message and continue
+        writeToOutputFile("Error: No integers were found on this line.\n")
+        continue
+    }
+    // Split the line into individual tokens
+    let lineTokens = line.split(separator: " ")
+    // Variable to store the sum of the integers
+    var sum = 0
+    // Initialize a flag variable
+    // If it's "none",
+    // it means that the line was processed without any errors.
+    var errorFlagString = "none"
+
+    // Loop through all the tokens in the line
+    for token in lineTokens {
+        // Convert the token to an integer and add it to the sum
+        if let number = Int(token) {
+            sum += number
+        } else {
+            // If the token is not an integer, break out of the loop
+            // Activate the flag to write an error instead of the sum
+            errorFlagString = "Error: \(token) is not an integer."
+            break
+        }
+    }
+
+    // Write the result
+    if errorFlagString == "none" {
+        // Write the sum if there were no errors
+        writeToOutputFile("\(sum)\n")
+    } else {
+        // Write the error message if there was an error
+        writeToOutputFile("\(errorFlagString)\n")
+    }
+}
